b722b18982d720785b55c9de60b6aae2f4d89cdb
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-27 16:14:27 -0600

    bug fixes
    yay

--- weather/weather.py
+++ weather/weather.py
@@ -1,11 +1,23 @@
 from redbot.core import commands
+import aiohttp
+import asyncio
 
 class Weather(commands.Cog):
     """Retreive weather for an area based on zip code (us only for now)"""
+
+     def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession()
 
     @commands.command()
     async def weather(self, ctx, zip_code: [str]):
         """Get the weather of a city/town by its zip code"""
         # Code:
-        response = requests.get("http://api.openweathermap.org/data/2.5/weather?zip=" + str(zip_code) + "&appid=168ced82a72953d81d018f75eec64aa0")
-        await ctx.send(response.content)
+        async def fetch(session, url):
+            async with session.get(url) as response:
+                return await response.text()
+
+        async def main():
+            async with aiohttp.ClientSession() as session:
+                response = await fetch(session, "http://api.openweathermap.org/data/2.5/weather?zip=" + str(zip_code) + "&appid=168ced82a72953d81d018f75eec64aa0")
+                await ctx.send(response.content)
