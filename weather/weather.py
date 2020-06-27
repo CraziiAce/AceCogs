6168e19066c843b1f740c88c703b7dffd4e28dc5
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-27 17:27:41 -0600

    update to have a better cog name

--- weather/weather.py
+++ weather/weather.py
@@ -2,8 +2,8 @@ from redbot.core import commands
 import aiohttp
 import asyncio
 
-class Weather(commands.Cog):
-    """Retreive weather for an area based on zip code (us only for now)"""
+class API(commands.Cog):
+    """Interact with various APIs"""
 
     def __init__(self, bot):
         self.bot = bot
@@ -13,6 +13,7 @@ class Weather(commands.Cog):
     async def weather(self, ctx, zip_code: int):
         """Get the weather of a city/town by its zip code"""
         # Code:
+        return zip_code
         async def fetch(session, url):
             async with session.get(url) as response:
                 return await response.text()
