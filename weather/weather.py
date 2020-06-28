00988066f7b8ba62b235480f0d2fb47de46e8391
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-28 16:33:02 -0600

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -22,12 +22,14 @@ class Weather(commands.Cog):
             url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip_code + "&appid=168ced82a72953d81d018f75eec64aa0&units=imperial"
             async with session.get(url) as response:
                 weather_response = await response.json()
-            #await ctx.send(f"\n__**Geographical info:**__ \nSpecified City: {weather_response['name']}\nLongitude: {weather_response['coord']['lon']}\nLatitude: {weather_response['coord']['lat']}\n__**Temperature**__ Info:\nCurrent Temp: {weather_response['main']['temp']}\nFeels Like: {weather_response['main']['feels_like']}\nDaily High: {weather_response['main']['temp_max']}\nDaily Low: {weather_response['main']['temp_min']}\n__**Wind Info:")
+            # await ctx.send(f"\n__**Geographical info:**__ \nSpecified City: {weather_response['name']}\nLongitude: {weather_response['coord']['lon']}\nLatitude: {weather_response['coord']['lat']}\n__**Temperature**__ Info:\nCurrent Temp: {weather_response['main']['temp']}\nFeels Like: {weather_response['main']['feels_like']}\nDaily High: {weather_response['main']['temp_max']}\nDaily Low: {weather_response['main']['temp_min']}\n__**Wind Info:")
             embed = discord.Embed(
-                    title=f"Weather in {weather_response['name']}",
-                    description=zip_code,
+                    title=f"Weather in {weather_response['name']}, {weather_response['sys']['country']}",
+                    description=weather_response['weather']['main'],
                     color=0x0276FD,
             )
             embed.add_field(name='Location:', value=f"**City:** {weather_response['name']}\n**Longitude:** {weather_response['coord']['lon']}\n **Latitude:** {weather_response['coord']['lat']}", inline=False)
-            embed.add_field(name='Temperature', value="Current Temp: {weather_response['main']['temp']}\nFeels Like: {weather_response['main']['feels_like']}\nDaily High: {weather_response['main']['temp_max']}\nDaily Low: {weather_response['main']['temp_min']}")
+            embed.add_field(name='Weather', value=f"Current Temp: {weather_response['main']['temp']}\nFeels Like: {weather_response['main']['feels_like']}\n**Daily High:** {weather_response['main']['temp_max']}\n**Daily Low:** {weather_response['main']['temp_min']}", inline=False)
+            embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weather_response['weather']['icon']}@2x.png")
+            embed.set_footer(text='Starry | discord.gg/7mSqpXN', icon_url='')
             await ctx.send(embed=embed)
