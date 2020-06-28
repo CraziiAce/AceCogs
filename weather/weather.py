c4655d5d04c87bb1ffe7df809633c7236d27dbe4
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-27 20:14:35 -0600

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -21,4 +21,4 @@ class Weather(commands.Cog):
         async with aiohttp.ClientSession() as session:
             response = await fetch(session, "http://api.openweathermap.org/data/2.5/weather?zip=" + int(zip_code) + "&appid=168ced82a72953d81d018f75eec64aa0")
     
-   await ctx.send(response.content)
+        await ctx.send(response.content)
