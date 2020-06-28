013f06552c3bf95fe8a54e2a2d64e1f35b0bb0fa
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-28 17:55:38 -0600

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -36,3 +36,24 @@ class Weather(commands.Cog):
             embed.set_footer(text='Starry | discord.gg/7mSqpXN', icon_url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
             await ctx.send(embed=embed)
             await ctx.send(sunriseTime)
+    @weather.command()
+    async def city(self, ctx, city_name:str):
+        """Get the weather of a city/town by its name"""
+        # Code:
+        async with aiohttp.ClientSession() as session:
+            url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=168ced82a72953d81d018f75eec64aa0&units=imperial"
+            async with session.get(url) as response:
+                weather_response = await response.json()
+                sunriseTime = datetime.datetime.utcfromtimestamp(weather_response['sys']['sunrise'])
+            # await ctx.send(f"\n__**Geographical info:**__ \nSpecified City: {weather_response['name']}\nLongitude: {weather_response['coord']['lon']}\nLatitude: {weather_response['coord']['lat']}\n__**Temperature**__ Info:\nCurrent Temp: {weather_response['main']['temp']}\nFeels Like: {weather_response['main']['feels_like']}\nDaily High: {weather_response['main']['temp_max']}\nDaily Low: {weather_response['main']['temp_min']}\n__**Wind Info:")
+            embed = discord.Embed(
+                    title=f"Weather in {weather_response['name']}, {weather_response['sys']['country']}",
+                    description=weather_response['weather'][0]['description'],
+                    color=0x0276FD,
+            )
+            embed.add_field(name='Location:', value=f"**City:** {weather_response['name']}\n**Longitude:** {weather_response['coord']['lon']}\n **Latitude:** {weather_response['coord']['lat']}", inline=False)
+            embed.add_field(name='Weather', value=f"**Current Temp:** {weather_response['main']['temp']}\n**Feels Like:** {weather_response['main']['feels_like']}\n**Daily High:** {weather_response['main']['temp_max']}\n**Daily Low:** {weather_response['main']['temp_min']}\n**Humidity:** {weather_response['main']['humidity']}%\n**Wind:** {weather_response['wind']['speed']}mph", inline=False)
+            embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
+            embed.set_footer(text='Starry | discord.gg/7mSqpXN', icon_url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
+            await ctx.send(embed=embed)
+            await ctx.send(sunriseTime)
