e75349e9b04f3634598f464ff7da424c77889b0a
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-27 20:13:23 -0600

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -13,12 +13,12 @@ class Weather(commands.Cog):
     async def weather(self, ctx, zip_code: int):
         """Get the weather of a city/town by its zip code"""
         # Code:
-        return zip_code
-        async def fetch(session, url):
-            async with session.get(url) as response:
-                return await response.text()
 
-        async def main():
-            async with aiohttp.ClientSession() as session:
-                response = await fetch(session, "http://api.openweathermap.org/data/2.5/weather?zip=" + int(zip_code) + "&appid=168ced82a72953d81d018f75eec64aa0")
-                await ctx.send(response.content)
+        
+        async with session.get(url) as response:
+            return await response.text()
+
+        async with aiohttp.ClientSession() as session:
+            response = await fetch(session, "http://api.openweathermap.org/data/2.5/weather?zip=" + int(zip_code) + "&appid=168ced82a72953d81d018f75eec64aa0")
+    
+   await ctx.send(response.content)
