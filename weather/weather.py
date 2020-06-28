8df5d140c21cf77f57c8bc875943202ff9a60161
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-28 08:31:57 -0600

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -16,6 +16,6 @@ class Weather(commands.Cog):
         # Code:
         async with aiohttp.ClientSession() as session:
             url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip_code + "&appid=168ced82a72953d81d018f75eec64aa0"
-             async with session.get(url) as response:
+            async with session.get(url) as response:
                 weather_response = await response.json()
                 await ctx.send("Longitude:" + weather_response["coord"]["lon"] + "\nLatitude:" + weather_response["coord"]["lat"])
