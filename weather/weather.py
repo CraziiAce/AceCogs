945a678796e4a4c590efc7d31a81ae241d1d23f4
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-29 13:27:39 -0600

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -23,10 +23,6 @@ class Weather(commands.Cog):
             url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip_code + "&appid=168ced82a72953d81d018f75eec64aa0&units=imperial"
             async with session.get(url) as response:
                 weather_response = await response.json()
-               # localSunrise = weather_response['sys']['sunrise'] + weather_response['timezone']
-                #sunriseTime = datetime.datetime.utcfromtimestamp(localSunrise)
-               # localTimeUnix = weather_response['dt'] - weather_response['timezone']
-                #localTime = datetime.datetime.utcfromtimestamp(localTimeUnix)
             # await ctx.send(f"\n__**Geographical info:**__ \nSpecified City: {weather_response['name']}\nLongitude: {weather_response['coord']['lon']}\nLatitude: {weather_response['coord']['lat']}\n__**Temperature**__ Info:\nCurrent Temp: {weather_response['main']['temp']}\nFeels Like: {weather_response['main']['feels_like']}\nDaily High: {weather_response['main']['temp_max']}\nDaily Low: {weather_response['main']['temp_min']}\n__**Wind Info:")
             embed = discord.Embed(
                     title=f"Weather in {weather_response['name']}, {weather_response['sys']['country']}",
@@ -47,11 +43,11 @@ class Weather(commands.Cog):
             url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=168ced82a72953d81d018f75eec64aa0&units=imperial"
             async with session.get(url) as response:
                 weather_response = await response.json()
-                #localSunrise = weather_response['sys']['sunrise'] + weather_response['timezone']
-                #sunriseTime = datetime.datetime.utcfromtimestamp(localSunrise)
-               # localTimeUnix = weather_response['dt'] - weather_response['timezone']
-               # localTime = datetime.datetime.utcfromtimestamp(localTimeUnix)
-               # timeSlice = slice(11, 16)
+                localSunrise = weather_response['sys']['sunrise'] + weather_response['timezone']
+                sunriseHour = datetime.hour.utcfromtimestamp(localSunrise)
+                sunriseMinute = datetime.minute.utcfromtimestamp(localSunrise)
+                localTimeUnix = weather_response['dt'] - weather_response['timezone']
+                localTime = datetime.datetime.utcfromtimestamp(localTimeUnix)
             # await ctx.send(f"\n__**Geographical info:**__ \nSpecified City: {weather_response['name']}\nLongitude: {weather_response['coord']['lon']}\nLatitude: {weather_response['coord']['lat']}\n__**Temperature**__ Info:\nCurrent Temp: {weather_response['main']['temp']}\nFeels Like: {weather_response['main']['feels_like']}\nDaily High: {weather_response['main']['temp_max']}\nDaily Low: {weather_response['main']['temp_min']}\n__**Wind Info:")
             embed = discord.Embed(
                     title=f"Weather in {weather_response['name']}, {weather_response['sys']['country']}",
@@ -60,7 +56,7 @@ class Weather(commands.Cog):
             )
             embed.add_field(name='Location:', value=f"**City:** {weather_response['name']}\n**Longitude:** {weather_response['coord']['lon']}\n **Latitude:** {weather_response['coord']['lat']}", inline=False)
             embed.add_field(name='Weather', value=f"**Current Temp:** {weather_response['main']['temp']}\n**Feels Like:** {weather_response['main']['feels_like']}\n**Daily High:** {weather_response['main']['temp_max']}\n**Daily Low:** {weather_response['main']['temp_min']}\n**Humidity:** {weather_response['main']['humidity']}%\n**Wind:** {weather_response['wind']['speed']}mph", inline=False)
-            #embed.add_field(name='Time', value=f"Local Time: {localTime['timeSlice']}")
+            embed.add_field(name='Time', value=f"**Local Time:** {localTime}\n Sunset Time: {sunriseHour}:{sunriseMinute")
             embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
             embed.set_footer(text='Starry | discord.gg/7mSqpXN', icon_url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
             await ctx.send(embed=embed)
