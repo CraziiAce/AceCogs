5e2c4fc90005cce8af5a6972e307c6fcf8cf303e
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-29 14:09:49 -0600

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -45,6 +45,8 @@ class Weather(commands.Cog):
                 weather_response = await response.json()
                 localSunrise = weather_response['sys']['sunrise'] + weather_response['timezone']
                 sunriseTime = datetime.datetime.utcfromtimestamp(localSunrise)
+                localSunset = weather_response['sys']['sunset'] + weather_response['timezone']
+                sunsetTime = datetime.datetime.utcfromtimestamp(localSunset)
                 localTimeUnix = weather_response['dt'] - weather_response['timezone']
                 localTime = datetime.datetime.utcfromtimestamp(localTimeUnix)
             # await ctx.send(f"\n__**Geographical info:**__ \nSpecified City: {weather_response['name']}\nLongitude: {weather_response['coord']['lon']}\nLatitude: {weather_response['coord']['lat']}\n__**Temperature**__ Info:\nCurrent Temp: {weather_response['main']['temp']}\nFeels Like: {weather_response['main']['feels_like']}\nDaily High: {weather_response['main']['temp_max']}\nDaily Low: {weather_response['main']['temp_min']}\n__**Wind Info:")
@@ -54,8 +56,8 @@ class Weather(commands.Cog):
                     color=0x0276FD,
             )
             embed.add_field(name='Location:', value=f"**City:** {weather_response['name']}\n**Longitude:** {weather_response['coord']['lon']}\n **Latitude:** {weather_response['coord']['lat']}", inline=False)
-            embed.add_field(name='Weather', value=f"**Current Temp:** {weather_response['main']['temp']}\n**Feels Like:** {weather_response['main']['feels_like']}\n**Daily High:** {weather_response['main']['temp_max']}\n**Daily Low:** {weather_response['main']['temp_min']}\n**Humidity:** {weather_response['main']['humidity']}%\n**Wind:** {weather_response['wind']['speed']}mph", inline=False)
-            embed.add_field(name='Time', value=f"**Local Time:** {localTime.strftime('%I:%M %p')}\n **Sunrise Time:** {sunriseTime.strftime('%I:%M %p')}")
+            embed.add_field(name='Weather', value=f"**🌡️ Current Temp:** {weather_response['main']['temp']}\n**🌡️ Feels Like:** {weather_response['main']['feels_like']}\n**🌡️ Daily High:** {weather_response['main']['temp_max']}\n**🌡️ Daily Low:** {weather_response['main']['temp_min']}\n**<:humidity:727253612778094683> Humidity:** {weather_response['main']['humidity']}%\n**🌬️ Wind:** {weather_response['wind']['speed']} mph", inline=False)
+            embed.add_field(name='Time', value=f"**🕓 Local Time:** {localTime.strftime('%I:%M %p')}\n **🌅 Sunrise Time:** {sunriseTime.strftime('%I:%M %p')}\n **🌇 Sunset Time:** {sunsetTime.strftime('%I:%M %p')}")
             embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
             embed.set_footer(text='Starry | discord.gg/7mSqpXN', icon_url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
             await ctx.send(embed=embed)
