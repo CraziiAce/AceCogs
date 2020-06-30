c6afd483447204ad8c292fb3b65219cd67252917
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-06-30 15:50:11 -0600

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -120,28 +120,28 @@ class Weather(commands.Cog):
                 'WI': 'Wisconsin',
                 'WY': 'Wyoming',
             }
-                async with aiohttp.ClientSession() as session:
-                    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name},{us_state_abbrev[state_code]}&appid=168ced82a72953d81d018f75eec64aa0&units=imperial"
-                    async with session.get(url) as response:
-                        weather_response = await response.json()
-                        if weather_response['cod'] == 404:
-                            await ctx.send("An error ocurred: city not found. Please make sure you put in the full state name.")
-                            pass
-                        localSunrise = weather_response['sys']['sunrise'] + weather_response['timezone']
-                        sunriseTime = datetime.datetime.utcfromtimestamp(localSunrise)
-                        localSunset = weather_response['sys']['sunset'] + weather_response['timezone']
-                        sunsetTime = datetime.datetime.utcfromtimestamp(localSunset)
-                        localTimeUnix = weather_response['dt'] + weather_response['timezone']
-                        localTime = datetime.datetime.utcfromtimestamp(localTimeUnix)
-                    embed = discord.Embed(
-                            title=f"Weather in {weather_response['name']}, {weather_response['sys']['country']}",
-                            url = f"https://openweathermap.org/city/{weather_response['sys']['id']}",
-                            description=weather_response['weather'][0]['description'],
-                            color=0x0276FD,
+            async with aiohttp.ClientSession() as session:
+                url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name},{us_state_abbrev[state_code]}&appid=168ced82a72953d81d018f75eec64aa0&units=imperial"
+                async with session.get(url) as response:
+                    weather_response = await response.json()
+                    if weather_response['cod'] == 404:
+                        await ctx.send("An error ocurred: city not found. Please make sure you put in the full state name.")
+                        pass
+                    localSunrise = weather_response['sys']['sunrise'] + weather_response['timezone']
+                    sunriseTime = datetime.datetime.utcfromtimestamp(localSunrise)
+                    localSunset = weather_response['sys']['sunset'] + weather_response['timezone']
+                    sunsetTime = datetime.datetime.utcfromtimestamp(localSunset)
+                    localTimeUnix = weather_response['dt'] + weather_response['timezone']
+                    localTime = datetime.datetime.utcfromtimestamp(localTimeUnix)
+                embed = discord.Embed(
+                    title=f"Weather in {weather_response['name']}, {weather_response['sys']['country']}",
+                    url = f"https://openweathermap.org/city/{weather_response['sys']['id']}",
+                    description=weather_response['weather'][0]['description'],
+                    color=0x0276FD,
                     )
-                    embed.add_field(name='Location:', value=f"**🏙️ City:** {weather_response['name']}\n**<:coordinates:727254888836235294> Longitude:** {weather_response['coord']['lon']}\n **<:coordinates:727254888836235294> Latitude:** {weather_response['coord']['lat']}", inline=False)
-                    embed.add_field(name='Weather', value=f"**🌡️ Current Temp:** {weather_response['main']['temp']}\n**🌡️ Feels Like:** {weather_response['main']['feels_like']}\n**🌡️ Daily High:** {weather_response['main']['temp_max']}\n**🌡️ Daily Low:** {weather_response['main']['temp_min']}\n**<:humidity:727253612778094683> Humidity:** {weather_response['main']['humidity']}%\n**🌬️ Wind:** {weather_response['wind']['speed']} mph", inline=False)
-                    embed.add_field(name='Time', value=f"**🕓 Local Time:** {localTime.strftime('%I:%M %p')}\n **🌅 Sunrise Time:** {sunriseTime.strftime('%I:%M %p')}\n **🌇 Sunset Time:** {sunsetTime.strftime('%I:%M %p')}")
-                    embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
-                    embed.set_footer(text='Starry | discord.gg/7mSqpXN', icon_url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
-                    await ctx.send(embed=embed)
+                embed.add_field(name='Location:', value=f"**🏙️ City:** {weather_response['name']}\n**<:coordinates:727254888836235294> Longitude:** {weather_response['coord']['lon']}\n **<:coordinates:727254888836235294> Latitude:** {weather_response['coord']['lat']}", inline=False)
+                embed.add_field(name='Weather', value=f"**🌡️ Current Temp:** {weather_response['main']['temp']}\n**🌡️ Feels Like:** {weather_response['main']['feels_like']}\n**🌡️ Daily High:** {weather_response['main']['temp_max']}\n**🌡️ Daily Low:** {weather_response['main']['temp_min']}\n**<:humidity:727253612778094683> Humidity:** {weather_response['main']['humidity']}%\n**🌬️ Wind:** {weather_response['wind']['speed']} mph", inline=False)
+                embed.add_field(name='Time', value=f"**🕓 Local Time:** {localTime.strftime('%I:%M %p')}\n **🌅 Sunrise Time:** {sunriseTime.strftime('%I:%M %p')}\n **🌇 Sunset Time:** {sunsetTime.strftime('%I:%M %p')}")
+                embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
+                embed.set_footer(text='Starry | discord.gg/7mSqpXN', icon_url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
+                await ctx.send(embed=embed)
