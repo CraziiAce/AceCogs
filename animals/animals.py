6fcff978decad06361d91977408903781d95028f
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-07-06 15:50:54 -0600

    Update animals.py

--- animals/animals.py
+++ animals/animals.py
@@ -21,14 +21,14 @@ class Animals(commands.Cog):
                 title = "Voilà! A random cat!",
                 color = embedColor,
             )
-            embed.set_image(url=response['url'])
+            embed.set_image(url=response['file'])
             embed.set_footer(text=f"Requested by {ctx.author.name}")
             await ctx.send(embed=embed)
     @commands.command(aliases = ['doggo', 'dogg', 'pupper'])
     async def dog(self, ctx):
         """Get a picture of a cute doggo."""
         async with aiohttp.ClientSession() as session:
-            url = "https://aws.random.cat/meow"
+            url = "https://dog.ceo/api/breeds/image/random"
             async with session.get(url) as response:
                 response = await response.json()
             embedColor = await ctx.embed_colour()
@@ -36,6 +36,6 @@ class Animals(commands.Cog):
                 title = "Voilà! A random dog!",
                 color = embedColor,
             )
-            embed.set_image(url=response['url'])
+            embed.set_image(url=response['message'])
             embed.set_footer(text=f"Requested by {ctx.author.name}")
             await ctx.send(embed=embed)
