431b94f04736367d0d4eba17a3332f975b87f386
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-07-15 12:40:22 -0600

    Update messagelog.py

--- messagelogs/messagelog.py
+++ messagelogs/messagelog.py
@@ -12,7 +12,7 @@ class MessageLog(commands.Cog):
     async def messagelog(self, ctx):
         """Tired of looking in audit logs if someone deleted a vulgar message? Use this in the channel you would like messages to be logged in, and they will all be logged."""
         guildID = ctx.guild.id
-        channelID = ctx.channel.id
+        channelID = ctx.channel
         self.message_log_pairs[guildID]=channelID
 
 
@@ -25,7 +25,6 @@ class MessageLog(commands.Cog):
                 description = f'Content: {message.content}',
                 color = embedColor,
             )
-            embed.set_thumbnail(url=response['url'])
             embed.set_footer(text=f"Sent by {message.author.name}", icon_url=message.author.avatar_url)
             await self.message_log_pairs[message.guild.id].send(embed=embed)
 
