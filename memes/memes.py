0c2fda55fb5129f6fcd4e0d6ea6d2819b59aec24
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-07-18 20:28:42 -0600

    Update memes.py

--- memes/memes.py
+++ memes/memes.py
@@ -19,7 +19,7 @@ class Memes(commands.Cog):
         """Get the dankest memes Reddit has to offer. Soon, you'll be able to specify by subreddit and number of memes"""
         api_key = await self.bot.get_shared_api_tokens("ksoftsi")
         if api_key.get("api_key") is None:
-            await ctx.send('The API key is not set. Set it with `set api ksoftsi api_key <your_api_key_here')
+            await ctx.send('The API key is not set. Set it with `set api ksoftsi api_key <your_api_key_here>`')
         else: 
             async with aiohttp.ClientSession() as session:
                 async with session.get('https://api.ksoft.si/images/random-meme', headers={"Authorization": f"Bearer {api_key.get('api_key')}"}) as resp:
@@ -93,7 +93,7 @@ class Memes(commands.Cog):
             while channelID in self.automeme_pairs:
                 api_key = await self.bot.get_shared_api_tokens("ksoftsi")
                 if api_key.get("api_key") is None:
-                    await ctx.send('The API key is not set. Set it with `set api ksoftsi api_key <your_api_key_here')
+                    await ctx.send('The API key is not set. Set it with `[p]set api ksoftsi api_key <your_api_key_here>`')
                 else: 
                     async with aiohttp.ClientSession() as session:
                         async with session.get('https://api.ksoft.si/images/random-meme', headers={"Authorization": f"Bearer {api_key.get('api_key')}"}) as resp:
@@ -119,3 +119,31 @@ class Memes(commands.Cog):
             await ctx.send(f"Automeme succesfuly turned off for <#{toggleID}>")
         else:
             await ctx.send("An unexpected error occurred. Are you sure automeme was set up for this channel?")
+    @commands.command
+    async def wikihow(self, ctx):
+        """Get a random Wikihow meme"""
+        api_key = await self.bot.get_shared_api_tokens("ksoftsi")
+        if api_key.get("api_key") is None:
+            await ctx.send('The API key is not set. Set it with `[p]set api ksoftsi api_key <your_api_key_here>`')
+        else: 
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://api.ksoft.si/images/random-wikihow', headers={"Authorization": f"Bearer {api_key.get('api_key')}"}) as resp:
+                    response = await resp.json()
+                embedColor = await ctx.embed_colour()
+                embed = discord.Embed(
+                    title = response['title'],
+                    url = response['article_url'],
+                    color = embedColor,
+                    description = f"Can't see the image? [Click Here.]({response['url']})"
+                )
+                embed.set_footer(text=f"{response['upvotes']} 👍 | {response['comments']} 💬")
+                embed.set_image(url=response['url'])
+                await ctx.send(embed=embed)
+    @commands.command
+    async def pornhub(self, ctx, text1, text2):
+        """Make text look like the pornhub logo."""
+        embedColor = await ctx.embed_colour()
+        embed = discord.Embed(
+            color = embedColor
+        embed.set_image(url=f"https://api.alexflipnote.dev/pornhub?text={text1}&text2={text2}")
+        await ctx.send(embed=embed)
