c42af95f1c820aa28b3e3c03722287c859c5b5f1
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-07-25 16:14:45 -0600

    Update finance.py
    switched apis bc finnhub returns json 10% of the time

--- finance/finance.py
+++ finance/finance.py
@@ -5,23 +5,23 @@ import discord
 
 class Finance(commands.Cog):
     """Get info about stocks/currencies/businesses"""
-    finnhub_base_url='https://finnhub.io/api/v1/'
+    tiingo_base_url='https://api.tiingo.com/api/'
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
     @commands.command()
     async def stock(self, ctx, *, stock_ticker:str):
         """Get the price of a US stock."""
-        api_key = await self.bot.get_shared_api_tokens("finnhub")
-        if api_key.get("api_key") is None:
-                return await ctx.send("The Finnhub API key has not been set. Please set it with `s!set api finnhub api_key <your api key>`")
+        token = await self.bot.get_shared_api_tokens("Tiingo")
+        if token.get("token") is None:
+                return await ctx.send("The Tiingo API key has not been set. Please set it with `s!set api tiingo token <your token>`")
         else:
             async with aiohttp.ClientSession() as session:
-                async with session.get(f'{self.finnhub_base_url}quote?symbol={stock_ticker}&') as resp:
+                async with session.get(f'{self.tiingo_base_url}iex/{stock_ticker}?token={token.get("token")}') as resp:
                     response = await resp.json()
                 if response == '{}':
                     await ctx.send('An unexpected error occured. Are you sure that is a valid, *US* stock ticker?')
-                percentage_change_a = response['c'] / response['o']
+                percentage_change_a = response[0]['last'] / response['open']
                 percentage_change_b = percentage_change_a - 1
                 percentage_change_final = percentage_change_b * 100
                 embedColor = await ctx.embed_colour()
@@ -33,8 +33,8 @@ class Finance(commands.Cog):
                     color = embedColor,
                 )
                 if percentage_gain:
-                    embed.add_field(name='Prices', value=f"Open: ${response['o']}\nHigh: ${response['h']}\nLow: ${response['l']}\nCurrent: ${response['c']}\nPercentage Gain: <:up_arrow:736390019136356442> %{percentage_change_final}")
+                    embed.add_field(name='Prices', value=f"Open: ${response[0]['open']}\nHigh: ${response[0]['high']}\nLow: ${response[0]['low']}\nCurrent: ${response[0]['last']}\nPercentage Gain: <:up_arrow:736390019136356442> %{percentage_change_final}")
                 if not percentage_gain:
-                     embed.add_field(name='Prices', value=f"Open: ${response['o']}\nHigh: ${response['h']}\nLow: ${response['l']}\nCurrent: ${response['c']}\nPercentage Loss: <:down_arrow:736390163839844422> %{percentage_change_final}")
-                embed.set_footer(text=f"Requested by {ctx.author.name} | Powered by finnhub.io")
+                    embed.add_field(name='Prices', value=f"Open: ${response[0]['open']}\nHigh: ${response[0]['high']}\nLow: ${response[0]['low']}\nCurrent: ${response[0]['last']}\nPercentage Loss: <:down_arrow:736390163839844422> %{percentage_change_final}")
+                embed.set_footer(text=f"Requested by {ctx.author.name} | Powered by tiingo.com")
             await ctx.send(embed=embed)
