d05253c1d05fe7a460e7b47801ebd2f65ff78f3b
LordWaffle27 <63609284+LordWaffle27@users.noreply.github.com>
2020-07-25 18:29:03 -0600

    Update finance.py

--- finance/finance.py
+++ finance/finance.py
@@ -5,25 +5,25 @@ import discord
 
 class Finance(commands.Cog):
     """Get info about stocks/currencies/businesses"""
-    tiingo_base_url='https://api.tiingo.com/api/'
+    iex_base_url='https://cloud.iexapis.com/stable'
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
     @commands.command()
     async def stock(self, ctx, *, stock_ticker:str):
         """Get the price of a US stock."""
-        token = await self.bot.get_shared_api_tokens("Tiingo")
+        token = await self.bot.get_shared_api_tokens("iex")
         if token.get("token") is None:
-                return await ctx.send("The Tiingo API key has not been set. Please set it with `s!set api tiingo token <your token>`")
+                return await ctx.send("The IEX API key has not been set. Please set it with `s!set api iex token <your token>`")
         else:
             async with aiohttp.ClientSession() as session:
-                async with session.get(f'{self.tiingo_base_url}iex/{stock_ticker}?token={token.get("token")}', params={
+                async with session.get(f'{self.iex_base_url}stock/{stock_ticker}/book?token={token.get("token")}', params={
                     "format":"json"
                 }) as resp:
                     response = await resp.json()
                 if response == '{}':
                     await ctx.send('An unexpected error occured. Are you sure that is a valid, *US* stock ticker?')
-                percentage_change_a = response[0]['last'] / response['open']
+                percentage_change_a = response['quote']['latestPrice'] / response['quote']['open']
                 percentage_change_b = percentage_change_a - 1
                 percentage_change_final = percentage_change_b * 100
                 embedColor = await ctx.embed_colour()
@@ -31,12 +31,12 @@ class Finance(commands.Cog):
                 if percentage_change_final < 0:
                     percentage_gain = False
                 embed = discord.Embed(
-                    title = f"Stock Data for {stock_ticker}",
+                    title = f"Stock Data for {response['quote']['companyName']}",
                     color = embedColor,
                 )
                 if percentage_gain:
-                    embed.add_field(name='Prices', value=f"Open: ${response[0]['open']}\nHigh: ${response[0]['high']}\nLow: ${response[0]['low']}\nCurrent: ${response[0]['last']}\nPercentage Gain: <:up_arrow:736390019136356442> %{percentage_change_final}")
+                    embed.add_field(name='Prices', value=f"Open: ${response['quote']['open']}\nHigh: ${response['quote']['high']}\nLow: ${response['quote']['low']}\nCurrent: ${response['quote']['latestPrice']}\nPercentage Gain: <:up_arrow:736390019136356442> %{percentage_change_final}")
                 if not percentage_gain:
-                    embed.add_field(name='Prices', value=f"Open: ${response[0]['open']}\nHigh: ${response[0]['high']}\nLow: ${response[0]['low']}\nCurrent: ${response[0]['last']}\nPercentage Loss: <:down_arrow:736390163839844422> %{percentage_change_final}")
-                embed.set_footer(text=f"Requested by {ctx.author.name} | Powered by tiingo.com")
+                    embed.add_field(name='Prices', value=f"Open: ${response['quote']['open']}\nHigh: ${response['quote']]['high']}\nLow: ${response['quote']['low']}\nCurrent: ${response['quote']['last']}\nPercentage Loss: <:down_arrow:736390163839844422> %{percentage_change_final}")
+                embed.set_footer(text=f"Requested by {ctx.author.name} | Powered by IEX Cloud")
             await ctx.send(embed=embed)
