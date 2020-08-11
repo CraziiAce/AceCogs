07cf73911d66397bb62762b33a3f7acd78d3cee1
CraziiAce <teddyjraz@gmail.com>
2020-08-11 14:46:14 -0600

    annoying

--- finance/finance.py
+++ finance/finance.py
@@ -23,17 +23,17 @@ class Finance(commands.Cog):
                     response = await resp.json()
                 if response == '{}':
                     await ctx.send('An unexpected error occured. Are you sure that is a valid, *US* stock ticker?')
-                percentage_change_a = response['quote']['latestPrice'] / response['quote']['open']
-                percentage_change_b = percentage_change_a - 1
-                percentage_change_final = percentage_change_b * 100
-                embedColor = await ctx.embed_colour()
-                percentage_gain = True
-                if percentage_change_final < 0:
+                #percentage_change_a = response['quote']['latestPrice'] / response['quote']['open']
+                #percentage_change_b = percentage_change_a - 1
+                #percentage_change_final = percentage_change_b * 100
+                #embedColor = await ctx.embed_colour()
+                #percentage_gain = True
+                #if percentage_change_final < 0:
                     percentage_gain = False
-                embed = discord.Embed(
-                    title = f"Stock Data for {response['quote']['companyName']}",
-                    color = embedColor,
-                )
+                #embed = discord.Embed(
+                    #title = f"Stock Data for {response['quote']['companyName']}",
+                    #color = embedColor,
+                #)
                 # if percentage_gain:
                     # embed.add_field(name='Prices', value=f"Open: ${response['quote']['open']}\nHigh: ${response['quote']['high']}\nLow: ${response['quote']['low']}\nCurrent: ${response['quote']['latestPrice']}\nPercentage Gain: <:up_arrow:736390019136356442> %{percentage_change_final}")
                 # if not percentage_gain:
